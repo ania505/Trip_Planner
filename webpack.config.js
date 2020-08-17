b0d38866560068cddedafb11ca792d742cb1f79b
Kelsey Schroeder <kelsey.schroeder@att.net>
2020-08-17 11:06:38 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const { NamedModulesPlugin } = require("webpack");
+
+module.exports = {
+    mode: "development",
+    entry: 'src/index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js'
+    }
+}
